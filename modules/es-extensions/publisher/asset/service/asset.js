55a2c191f71c4072e5bb19ecf3592edad7ff51f3
shazni <mshazninazeer@gmail.com>
2014-11-26 19:31:31 +0530

    Adding WSDL Addition and listing

--- modules/es-extensions/publisher/asset/service/asset.js
+++ modules/es-extensions/publisher/asset/service/asset.js
@@ -1,6 +1,89 @@
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 asset.manager=function(ctx){
+    var addWSDL = function(name, version, url){
+            var userMod = require('store').user;
+            var userRegistry = userMod.userRegistry(ctx.session);
+            
+            var utils = Packages.org.wso2.carbon.registry.resource.services.utils.ImportResourceUtil;
 
-   var createArtifact = function (manager, options) {
+            var parentPath = "/_system/governance/trunk/wsdls/".concat(version);
+            var mediaType = "application/wsdl+xml";
+
+            var javaArray = Packages.java.lang.reflect.Array;
+            var properties = javaArray.newInstance(java.lang.String, 1, 2);
+
+            properties[0][0] = 'version';
+            properties[0][1] = version;
+
+            utils.importResource(parentPath, name, mediaType, '', url, '', userRegistry.registry, properties);
+    };
+
+    var setAttributes = function(artifact, attributes) {
+        for (name in attributes) {
+            if (attributes.hasOwnProperty(name)) {
+                attribute = attributes[name];
+                if (attribute instanceof Array) {
+                    artifact.setAttributes(name, attribute);
+                } else {
+                    artifact.setAttribute(name, attribute);
+                }
+            }
+        }
+    }
+
+    var setId = function(artifact, id) {
+        if (id) {
+            artifact.id = id;
+        }    
+    }
+
+    var setContent = function(artifact, content) {
+        if (content) {
+            if (content instanceof Stream) {
+                artifact.setContent(IOUtils.toByteArray(content.getStream()));
+            } else {
+                artifact.setContent(new java.lang.String(content).getBytes());
+            }
+        }  
+    }
+
+    var createOMContent = function(attributes) {
+        var omContent = "<metadata xmlns=\"http://www.wso2.org/governance/metadata\"><overview><name>";
+        omContent += attributes.overview_name;
+        omContent += "</name><namespace>";
+
+        omContent += attributes.overview_namespace;
+
+        omContent += "</namespace><version>";
+        omContent += attributes.overview_version;
+        omContent += "</version></overview>";
+
+        if(attributes.interface_wsdlUrl) {
+            omContent += "<interface><wsdlURL>";
+            omContent += attributes.interface_wsdlUrl;
+            omContent += "</wsdlURL></interface>";
+        }
+
+        omContent += "</metadata>";
+
+        return omContent;
+    }
+
+    var createArtifact = function (manager, options) {
 
         var QName = Packages.javax.xml.namespace.QName;
         var IOUtils = Packages.org.apache.commons.io.IOUtils;
@@ -15,31 +98,23 @@ asset.manager=function(ctx){
             log.info('Keys: '+key)
         }
 
-        var artifact = manager.newGovernanceArtifact(qName);
         var attributes = options.attributes;
+
+        // var artifact = manager.newGovernanceArtifact(options.name, options.attributes.overview_namespace,
+        //     options.attributes.overview_version, options.attributes.interface_wsdlUrl);
+        var omContent = createOMContent(attributes);
+        log.info(omContent);
+        var artifact = manager.newGovernanceArtifact(omContent);
+        // var artifact = manager.newGovernanceArtifact(qName);
 
         log.info('Finished creating Governance Artifact');
         
-        for (name in attributes) {
-            if (attributes.hasOwnProperty(name)) {
-                attribute = attributes[name];
-                if (attribute instanceof Array) {
-                    artifact.setAttributes(name, attribute);
-                } else {
-                    artifact.setAttribute(name, attribute);
-                }
-            }
-        }
-        if (options.id) {
-            artifact.id = options.id;
-        }
-        if (options.content) {
-            if (options.content instanceof Stream) {
-                artifact.setContent(IOUtils.toByteArray(options.content.getStream()));
-            } else {
-                artifact.setContent(new java.lang.String(options.content).getBytes());
-            }
-        }
+        setAttributes(artifact, attributes);
+
+        setId(artifact, options.id);
+
+        setContent(artifact, options.content);
+
         lc = options.lifecycles;
         if (lc) {
             length = lc.length;
@@ -52,15 +127,19 @@ asset.manager=function(ctx){
 
 
 	return{
+        get:function(id){
+            var asset = this._super.get.call(this,id);
+            return asset;
+        },
 		create:function(options){
 			var log=new Log();
 			log.info('Service create method called!');
 			var manager=this.am.manager;
             log.info('Calling addGenericArtifact');
             log.info('Options: '+ stringify(options));
+
             var artifact=createArtifact(manager, options);
-			manager.addGenericArtifact(artifact);		
-            // createArtifact(manager, options);       
+			manager.addGenericArtifact(artifact);
 			log.info('Service create method ended!');
             options.id=artifact.getId();
 		},
@@ -70,8 +149,9 @@ asset.manager=function(ctx){
             var manager=this.am.manager;
             log.info('Calling updateGenericArtifact');
             log.info('Options: '+ stringify(options));
+
             var artifact=createArtifact(manager, options);
-            manager.updateGenericArtifact(artifact);            
+            manager.updateGenericArtifact(artifact);    
             log.info('Service update method ended!');
             options.id=artifact.getId();
         }
@@ -208,3 +288,14 @@ asset.renderer = function(ctx) {
         }
     };
 };
+
+/*
+asset.configure = function() {
+    return {
+        meta: {
+            lifecycle: {
+                name: 'ServiceLifeCycle'
+            }
+        }
+    };
+};*/
