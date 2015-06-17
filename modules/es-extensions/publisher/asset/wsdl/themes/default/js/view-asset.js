78d0d70bdd35af6ad24d7d12dd2a8c16c7419e29
prasa7 <prasannadangalla@gmail.com>
2015-06-18 00:17:45 +0530

    Added ui fix for firefox in diff-view

--- modules/es-extensions/publisher/asset/wsdl/themes/default/js/view-asset.js
+++ modules/es-extensions/publisher/asset/wsdl/themes/default/js/view-asset.js
@@ -45,11 +45,11 @@ $(function() {
         }
     });
 });
-function viewDiffView(path) {
-    var diff_view_url = path + "/pages/diff?type=wsdl&path=" + $("#diff-view-version").val() + ',' +
+$("#diff-view-version").on('change',function(){
+    var diff_view_url = "/publisher/pages/diff?type=wsdl&path=" + $("#diff-view-version").val() + ',' +
         $("#diff-view-version").find(':selected').data('base_path');
-    window.open(diff_view_url, "_blank");
-}
+    $("#diff-view-button").attr("href", diff_view_url);
+});
 
 $('select.select2').select2({
     dropdownCssClass: 'version-select-drop',
