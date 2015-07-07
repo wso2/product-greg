f278b52f17f6dea73a1ca862ce11058690c446f9
shazni <mshazninazeer@gmail.com>
2015-07-07 22:42:42 +0530

    Improvement done for REGISTRY-2638

--- modules/es-extensions/publisher/asset/wsdl/asset.js
+++ modules/es-extensions/publisher/asset/wsdl/asset.js
@@ -67,6 +67,40 @@ asset.manager = function(ctx) {
             registry.put(path, wsdlResource);
         }
     };
+
+    var getAssociations = function(associatedResources, userRegistry){
+        //Array to store the association names.
+        var associations = [];
+
+        for(var index = 0; index< associatedResources.length; index++){
+            var deps = {};
+            var path = associatedResources[index].getDestinationPath();
+            var subPaths = path.split('/');
+            var associationTypePlural = subPaths[4];
+            var associationName = subPaths[subPaths.length - 1];
+            var resource = userRegistry.registry.get(path);
+            var associationUUID = resource.getUUID();
+            deps.associationName = associationName;
+            deps.associationType = associationTypePlural.substring(0, associationTypePlural.lastIndexOf('s'));
+            deps.associationUUID = associationUUID;
+
+            if(deps.associationType == "soapservice") {
+                associations.push(deps);
+            }
+        }
+        return associations;
+    };
+
+    var setDependencies = function(asset,userRegistry) {
+        try {
+            //get dependencies of the artifact.
+            var associatedResources = userRegistry.registry.getAllAssociations(asset.path);
+            asset.dependencies = getAssociations(associatedResources, userRegistry);
+        } catch(e) {
+            asset.dependencies = [];
+        }
+    };
+
     return {
         importAssetFromHttpRequest: function(options) {
             log.debug('Importing asset from request');
@@ -118,6 +152,9 @@ asset.manager = function(ctx) {
                 var content = resource.getContent();
                 var value = '' + new Stream(new ByteArrayInputStream(content));
                 item.content = value;
+
+                var userRegistry = getRegistry(ctx.session);
+                setDependencies(item, userRegistry);
             } catch(e) {
                 log.error(e);
                 return null;
