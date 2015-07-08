7f898acf69a85724b2731033821ec5e5df14426b
shazni <mshazninazeer@gmail.com>
2015-07-08 18:24:29 +0530

    Fix for REGISTRY-2648

---
+++ modules/es-extensions/publisher/asset/soapservice/themes/default/helpers/create_asset.js
@@ -0,0 +1,5 @@
+var resources = function (page, meta) {
+    return {
+       	js:['create_asset_addons.js']
+    };
+};
---
+++ modules/es-extensions/publisher/asset/soapservice/themes/default/js/create_asset_addons.js
@@ -0,0 +1,7 @@
+$(function(){
+	$('h2.field-title').each(function(){
+		if($(this).next().attr('id') != "collapseoverview"){
+			$(this).next().removeClass('in');
+		}
+	})
+});
