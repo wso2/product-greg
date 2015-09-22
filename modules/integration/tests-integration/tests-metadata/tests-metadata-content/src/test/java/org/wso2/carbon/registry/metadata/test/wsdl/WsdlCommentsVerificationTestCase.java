f1c0a68e91af2658063211b448cd6e8ee215c72c
cnapagoda <cnapagoda@gmail.com>
2015-09-22 21:19:35 +0530

    Fix Integration test failures

--- modules/integration/tests-integration/tests-metadata/tests-metadata-content/src/test/java/org/wso2/carbon/registry/metadata/test/wsdl/WsdlCommentsVerificationTestCase.java
+++ modules/integration/tests-integration/tests-metadata/tests-metadata-content/src/test/java/org/wso2/carbon/registry/metadata/test/wsdl/WsdlCommentsVerificationTestCase.java
@@ -121,7 +121,8 @@ public class WsdlCommentsVerificationTestCase extends GREGIntegrationBaseTest{
         
         GovernanceArtifact[] governanceArtifacts = wsdl.getDependents();
         for (GovernanceArtifact tmpGovernanceArtifact : governanceArtifacts) {
-            if (!tmpGovernanceArtifact.getPath().contentEquals(previousGovernanceArtifactPath)) {
+            if (tmpGovernanceArtifact != null && tmpGovernanceArtifact.getPath() != null &&
+                !tmpGovernanceArtifact.getPath().contentEquals(previousGovernanceArtifactPath)) {
                 wsRegistry.delete(pathPrefix + tmpGovernanceArtifact.getPath());
             }
             previousGovernanceArtifactPath = tmpGovernanceArtifact.getPath();
@@ -130,7 +131,9 @@ public class WsdlCommentsVerificationTestCase extends GREGIntegrationBaseTest{
         for (Endpoint tmpEndpoint : endpoints) {
         	GovernanceArtifact[] dependentArtifacts =  tmpEndpoint.getDependents();
         	for (GovernanceArtifact tmpGovernanceArtifact : dependentArtifacts) {
-                wsRegistry.delete(pathPrefix + tmpGovernanceArtifact.getPath());
+                if (tmpGovernanceArtifact != null && tmpGovernanceArtifact.getPath() != null) {
+                    wsRegistry.delete(pathPrefix + tmpGovernanceArtifact.getPath());
+                }
             }
             wsRegistry.delete(pathPrefix + tmpEndpoint.getPath());
         }
