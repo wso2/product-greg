e5957674b60eb3d8fa797954b2aed03ca2be55df
Chanaka Jayasena <chanaka@wso2.com>
2015-10-23 19:08:09 +0530

    Fixing validation for schema

--- modules/es-extensions/publisher/asset/schema/themes/default/js/create_form.js
+++ modules/es-extensions/publisher/asset/schema/themes/default/js/create_form.js
@@ -23,6 +23,8 @@
  * ]
  */
 $(function() {
+    var uploadUI,importUI;
+
     //function to set the schema name from the schema url.
     $('input[name="overview_url"]').change(function() {
         var schemaUrl = $('input[name="overview_url"]').val();
@@ -50,125 +52,91 @@ $(function() {
         }
         $('input[name="overview_name"]').val(schemaFileName);
     });
-    //function to display upload or import uis.
-    var uploadUI = $('#uploadUI');
-    var importUI = $('#importUI');
-    importUI.show();
 
-    $('select').on('change', function() {
-        var addSelector = $('#addMethodSelector');
-        var selectedValue = addSelector.val();
-        if (selectedValue == "upload") {
-            uploadUI.show();
-            importUI.hide();
-        } else if (selectedValue == "import") {
-            importUI.show();
-            uploadUI.hide();
-        }
-    });
+    $('#form-asset-create').ajaxForm({
+        beforeSubmit:function(){
+            var action = "";
+            if ($('#importUI').is(":visible")) {
+                action = "addNewAssetButton";
+            } else if ($('#uploadUI').is(":visible")) {
+                action = "addNewSchemaFileAssetButton";
+            }
+            if (action === 'addNewSchemaFileAssetButton') {//upload via file browser
+                //call the custom endpoint for processing schema upload via file browser.
+                var $schemaFileInput = $('input[name="schema_file"]');
+                var schemaFileInputValue = $schemaFileInput.val();
+                var schemaFilePath = schemaFileInputValue;
 
-    var obtainFormMeta=function(formId){
-        return $(formId).data();
-    };
-
-    var doSubmit = function(action,container){
-        $('#form-asset-create').ajaxForm({
-            success:function(){
-                var options=obtainFormMeta('#form-asset-create');
-                window.location=options.redirectUrl;
-                messages.alertSuccess("Successfully created the schema");
-                $('form[name="form-asset-create"]').data('submitted', false);
-            },
-            error:function(){
-                messages.alertError("Error occurred whilw adding the schema");
-
-                var createButton = "";
-                if(action === 'addNewSchemaFileAssetButton') {
-                    createButton = $('#btn-create-asset-file');
-                } else if(action === 'addNewAssetButton') {
-                    createButton = $('#btn-create-asset');
+                if(!validator.isValidForm(uploadUI)) {
+                    messages.alertInfo("All required fields must be provided");
+                    return false;
                 }
+                var fileName = schemaFilePath.split('\\').reverse()[0];
+                //set the zip file name, to the hidden attribute.
+                $('#schema_file_name').val(fileName);
+            } else if (action === 'addNewAssetButton') {//upload via url.
+                if(!validator.isValidForm(importUI)) {
+                    messages.alertInfo("All required fields must be provided");
+                    return false;
+                }
+            }
 
-                createButton.show();
-                createButton.next().show();
-                $('.fa-spinner').parent().remove();
-                $('form[name="form-asset-create"]').data('submitted', false);
-            }   
-        });
-    };
-
-    var styleFix = function(){
-        var item = $('#ui-asset-operations-overlay');
-        item.css('top','-15px');
-    };
-
-    //function to call the custom schema api or default api.
-    $('form[name="form-asset-create"] input[type="submit"]').click(function(event) {
-        var action = "";
-        if ($('#importUI').is(":visible")) {
-            action = "addNewAssetButton";
-        } else if ($('#uploadUI').is(":visible")) {
-            action = "addNewSchemaFileAssetButton";
+            var createButton = $('#form-asset-create input[type="submit"]');
+            createButton.attr('disabled', 'disabled');
+            createButton.next().attr('disabled', 'disabled');
+            caramel.render('loading', 'Creating asset. Please wait..', function (info, content) {
+                var $content = $(content).removeClass('loading-animation-big').addClass('loading-animation');
+                createButton.parent().append($content);
+            });
+        },
+        success:function(data){
+            //$.cookie("new-asset-" + data.type, data.id + ":" + data.type + ":" + data.name);
+            window.location=$('#form-asset-create').attr('data-redirect-url');
+            messages.alertSuccess("Successfully created the schema");
+        },
+        error:function(){
+            messages.alertError("Error occurred while adding the schema");
+            var createButton = $('#form-asset-create input[type="submit"]');
+            createButton.removeAttr('disabled');
+            $('.fa-spinner').parent().remove();
         }
-
-        var container;
-        
-        var $form = $('form[name="form-asset-create"]');
-        if (action === 'addNewSchemaFileAssetButton') {//upload via file browser
-            //call the custom endpoint for processing schema upload via file browser.
-            $form.attr('action', caramel.context + '/assets/schema/apis/schemas');
-            var $schemaFileInput = $('input[name="schema_file"]');
-            var schemaFileInputValue = $schemaFileInput.val();
-            var schemaFilePath = schemaFileInputValue;
-
-            var schemaFileVersion = $('input[name="file_version"]').val();
-            if(schemaFileVersion == "" || schemaFilePath == "") {
-                messages.alertInfo("All required fields must be provided");
-                return false;
-            }
-
-            if($form.data('submitted') === true) {
-                return false;
-            } else {
-                $form.data('submitted', true);
-            }
-
-            var fileName = schemaFilePath.split('\\').reverse()[0];
-            //set the zip file name, to the hidden attribute.
-            $('input[name="schema_file_name"]').val(fileName);
-            container = 'saveButtonsFile';
-        } else if (action === 'addNewAssetButton') {//upload via url.
-            var schemaUrl = $('input[name="overview_url"]').val();
-            var schemaName = $('input[name="overview_name"]').val();
-            var schemaVersion = $('input[name="overview_version"]').val();
-
-            if(schemaUrl == "" || schemaName == "" || schemaVersion == "") {
-                messages.alertInfo("All required fields must be provided");
-                return false;
-            }
-
-            if($form.data('submitted') === true) {
-                return false;
-            } else {
-                $form.data('submitted', true);
-            }
-
-            //call the default endpoint.
-            $form.attr('action', caramel.context + '/apis/assets?type=schema');
-            container = 'saveButtonsURL';
-        }
-
-        doSubmit(action, container);
-
-        var createButton = "";
-        if(action === 'addNewSchemaFileAssetButton') {
-            createButton = $('#btn-create-asset-file');
-        } else if(action === 'addNewAssetButton') {
-            createButton = $('#btn-create-asset');
-        }
-        
-        createButton.hide();
-        createButton.next().hide();
-        createButton.parent().append($('<div style="font-size: 16px;margin-top: 10px;"><i class="fa fa-spinner fa-pulse"></i> Creating the schema instance...</div>'));
     });
+    var initUI = function() {
+        //function to display upload or import uis.
+        uploadUI = $('#uploadUI');
+        importUI = $('#importUI');
+        importUI.show();
+        validator.initValidationEvents('importUI',function(){});
+        $('#form-asset-create').attr('action', caramel.context + '/apis/assets?type=schema');
+
+        $('#addMethodSelector').val('import');
+        $('select').on('change', function() {
+            var addSelector = $('#addMethodSelector');
+            var selectedValue = addSelector.val();
+            if (selectedValue == "upload") {
+                uploadUI.show();
+                importUI.hide();
+                validator.initValidationEvents(uploadUI,function(){});
+                validator.removeValidationEvents(importUI);
+                $('#form-asset-create').attr('action', caramel.context + '/assets/schema/apis/schemas');
+
+            } else if (selectedValue == "import") {
+                importUI.show();
+                uploadUI.hide();
+                validator.initValidationEvents(importUI,function(){});
+                validator.removeValidationEvents(uploadUI);
+                $('#form-asset-create').attr('action', caramel.context + '/apis/assets?type=schema');
+            }
+        });
+        var fileInput = $('input[name="schema_file"]');
+        if(fileInput.val().length > 0){
+            var fileName = fileInput.val().split('\\').reverse()[0];
+            $('#schema_file_name').val(fileName);
+        }
+        $('input[name="schema_file"]').change(function(){
+            var fileName = fileInput.val().split('\\').reverse()[0];
+            $('#schema_file_name').val(fileName);
+        })
+    };
+    initUI();
 });
