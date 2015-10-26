db16287bd2f6c90f0c5bfc8719119650c300068f
dharshanaw <dharshanaw@wso2.com>
2015-10-26 17:01:41 +0530

    Adding Greg ES rest integration base

---
+++ modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/publisher/GregRestResourceLifeCycleManagementTestCase.java
@@ -0,0 +1,180 @@
+/*
+*Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+package org.wso2.carbon.registry.es.publisher;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.wink.client.ClientResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.engine.exceptions.AutomationFrameworkException;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
+import org.wso2.carbon.automation.extensions.servers.carbonserver.CarbonServerManager;
+import org.wso2.carbon.automation.extensions.servers.carbonserver.MultipleServersManager;
+import org.wso2.carbon.governance.generic.stub.ManageGenericArtifactServiceRegistryExceptionException;
+import org.wso2.carbon.governance.lcm.stub.LifeCycleManagementServiceExceptionException;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.registry.resource.stub.ResourceAdminServiceExceptionException;
+import org.wso2.carbon.registry.server.mgt.test.ServerStartupTestCase;
+import org.wso2.greg.integration.common.clients.LifeCycleManagementClient;
+import org.wso2.greg.integration.common.clients.ManageGenericArtifactAdminServiceClient;
+import org.wso2.greg.integration.common.clients.ResourceAdminServiceClient;
+import org.wso2.greg.integration.common.utils.GREGIntegrationBaseTest;
+import org.wso2.greg.integration.common.utils.GenericRestClient;
+
+import javax.activation.DataHandler;
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GregRestResourceLifeCycleManagementTestCase extends GREGIntegrationBaseTest {
+    private static final Log log =
+            LogFactory.getLog(GregRestResourceLifeCycleManagementTestCase.class);
+    private TestUserMode userMode;
+    String jSessionId;
+    String assetId;
+    String cookieHeader;
+    GenericRestClient genericRestClient;
+    Map<String, String> queryParamMap;
+    Map<String, String> headerMap;
+    String publisherUrl;
+    String resourceRegistryPath;
+    ManageGenericArtifactAdminServiceClient manageGenericArtifactAdminServiceClient;
+    LifeCycleManagementClient lifeCycleAdminServiceClient;
+    ResourceAdminServiceClient resourceAdminServiceClient;
+    String resourcePath;
+
+    @Factory(dataProvider = "userModeProvider")
+    public GregRestResourceLifeCycleManagementTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+        super.init(userMode);
+        genericRestClient = new GenericRestClient();
+        publisherUrl = automationContext.getContextUrls()
+                .getSecureServiceUrl().replace("services", "publisher/apis");
+        manageGenericArtifactAdminServiceClient =
+                new ManageGenericArtifactAdminServiceClient(backendURL, sessionCookie);
+        lifeCycleAdminServiceClient = new LifeCycleManagementClient(backendURL, sessionCookie);
+        resourceAdminServiceClient = new ResourceAdminServiceClient(backendURL, sessionCookie);
+        resourcePath = FrameworkPathUtil.getSystemResourceLocation()
+                       + "artifacts" + File.separator + "GREG" + File.separator;
+        resourceRegistryPath = "/_system/governance/repository/components" +
+                               "/org.wso2.carbon.governance/types/updated-serviceLC.rxt";
+    }
+    @BeforeMethod
+    public void resetParameters()
+    {
+        queryParamMap = new HashMap<String, String>();
+        headerMap = new HashMap<String, String>();
+    }
+
+    @Test(groups = {"wso2.greg", "wso2.greg.es"}, description = "Add lifecycle for rest resources" ,
+            dependsOnMethods = {"authenticatePublisher"})
+    public void addLifecycleForRestServiceResources()
+            throws IOException, LifeCycleManagementServiceExceptionException,
+                   ManageGenericArtifactServiceRegistryExceptionException, InterruptedException,
+                   ResourceAdminServiceExceptionException, JSONException,
+                   AutomationFrameworkException {
+        lifeCycleAdminServiceClient.addLifeCycle(readFile(resourcePath + "lifecycle"
+                                                          + File.separator + "updated-serviceLC.xml"));
+        DataHandler dh = new DataHandler(new URL("file:///" + resourcePath + "rxt"
+                                                 + File.separator + "updated-serviceLC.rxt"));
+        resourceAdminServiceClient.addResource(resourceRegistryPath,
+                                               "application/vnd.wso2.registry-ext-type+xml",
+                                               "TstDec", dh);
+        getAllLifeCycles();
+
+
+    }
+
+    @Test(groups = {"wso2.greg", "wso2.greg.es"}, description = "Authenticate Publisher test",
+            dependsOnMethods = {"addLifecycleForRestServiceResources", "authenticatePublisher"})
+    public void createRestServiceAsset() throws JSONException, InterruptedException, IOException {
+        queryParamMap.put("type", "restservice");
+        String dataBody = readFile(resourcePath + "json" + File.separator
+                                   + "publisherPublishRestResource.json");
+        ClientResponse response =
+                genericRestClient.geneticRestRequestPost(publisherUrl + "/assets",
+                                                         MediaType.APPLICATION_JSON,
+                                                         MediaType.APPLICATION_JSON, dataBody
+                        , queryParamMap, headerMap, cookieHeader);
+        JSONObject obj = new JSONObject(response.getEntity(String.class));
+        Assert.assertTrue((response.getStatusCode() == 201),
+                          "Wrong status code ,Expected 201 Created ,Received " +
+                          response.getStatusCode());
+        assetId = obj.get("id").toString();
+        Assert.assertNotNull(assetId, "Empty asset resource id available" +
+                                      response.getEntity(String.class));
+        Thread.sleep(100000000);
+
+    }
+
+    @Test(groups = {"wso2.greg", "wso2.greg.es"}, description = "Authenticate Publisher test")
+    public void authenticatePublisher() throws JSONException {
+        ClientResponse response =
+                genericRestClient.geneticRestRequestPost(publisherUrl + "/authenticate/",
+                                                         MediaType.APPLICATION_FORM_URLENCODED,
+                                                         MediaType.APPLICATION_JSON,
+                                                         "username=admin&password=admin"
+                        , queryParamMap, headerMap, null);
+        JSONObject obj = new JSONObject(response.getEntity(String.class));
+        Assert.assertTrue((response.getStatusCode() == 200),
+                          "Wrong status code ,Expected 200 OK ,Received " +
+                          response.getStatusCode());
+        jSessionId = obj.getJSONObject("data").getString("sessionId");
+        cookieHeader = "JSESSIONID=" + jSessionId;
+        Assert.assertNotNull(jSessionId, "Invalid JSessionID received");
+    }
+
+
+
+    @AfterClass(alwaysRun = true)
+    public void cleanUp() throws RegistryException {
+
+    }
+
+    @DataProvider
+    private static TestUserMode[][] userModeProvider() {
+        return new TestUserMode[][]{
+                new TestUserMode[]{TestUserMode.SUPER_TENANT_ADMIN},
+//                new TestUserMode[]{TestUserMode.TENANT_USER},
+        };
+    }
+
+    private JSONObject getAllLifeCycles() throws JSONException {
+        ClientResponse response =
+                genericRestClient.geneticRestRequestGet
+                        (publisherUrl+"/lifecycles"
+                         , queryParamMap, headerMap, cookieHeader);
+     return new JSONObject(response.getEntity(String.class));
+    }
+}
