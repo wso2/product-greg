e69fabb9847a55e900d7a61c2818502fbc443199
dharshanaw <dharshanaw@wso2.com>
2015-11-17 16:54:58 +0530

    Adding new XSD and adding tests as classes

--- modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/publisher/generic/GregRestResourceTestCase.java
+++ modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/publisher/generic/GregRestResourceTestCase.java
@@ -15,7 +15,7 @@
 *specific language governing permissions and limitations
 *under the License.
 */
-package org.wso2.carbon.registry.es.publisher;
+package org.wso2.carbon.registry.es.publisher.generic;
 
 public class GregRestResourceTestCase {
 }
