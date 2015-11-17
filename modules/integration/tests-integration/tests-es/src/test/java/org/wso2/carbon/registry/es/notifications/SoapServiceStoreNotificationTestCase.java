4f05beeee96d05b0a692c018db519988d5cc1030
Rajith <rajithroshan90@gmail.com>
2015-11-17 16:52:47 +0530

    Adding Store Side Test cases for Notifications

---
+++ modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/notifications/SoapServiceStoreNotificationTestCase.java
@@ -0,0 +1,196 @@
+/*
+*Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+
+package org.wso2.carbon.registry.es.notifications;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.wink.client.ClientResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.*;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
+import org.wso2.greg.integration.common.utils.GREGIntegrationBaseTest;
+import org.wso2.greg.integration.common.utils.GenericRestClient;
+
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * This class testes subscription & notification for soap services on store notification
+ */
+public class SoapServiceStoreNotificationTestCase extends GREGIntegrationBaseTest {
+
+    private static final Log log = LogFactory.getLog(SoapServiceStoreNotificationTestCase.class);
+
+    private TestUserMode userMode;
+    String jSessionIdPublisher;
+    String jSessionIdStore;
+    String assetId;
+    String cookieHeaderPublisher;
+    String cookieHeaderStore;
+    GenericRestClient genericRestClient;
+    Map<String, String> queryParamMap;
+    Map<String, String> headerMap;
+    String publisherUrl;
+    String storeUrl;
+    String resourcePath;
+
+    @Factory(dataProvider = "userModeProvider")
+    public SoapServiceStoreNotificationTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+        super.init(userMode);
+        genericRestClient = new GenericRestClient();
+        queryParamMap = new HashMap<String, String>();
+        headerMap = new HashMap<String, String>();
+        resourcePath = FrameworkPathUtil.getSystemResourceLocation()
+                + "artifacts" + File.separator + "GREG" + File.separator;
+        publisherUrl = automationContext.getContextUrls()
+                .getSecureServiceUrl().replace("services", "publisher/apis");
+        storeUrl = automationContext.getContextUrls()
+                .getSecureServiceUrl().replace("services", "store/apis");
+        setTestEnvironment();
+    }
+
+    @Test(groups = { "wso2.greg",
+            "wso2.greg.es" }, description = "Adding subscription to soap service on LC state change")
+    public void addSubscriptionToLcStateChange() throws JSONException, IOException {
+
+        JSONObject dataObject = new JSONObject();
+        dataObject.put("notificationType", "StoreLifeCycleStateChanged");
+        dataObject.put("notificationMethod", "work");
+
+        ClientResponse response = genericRestClient
+                .geneticRestRequestPost(storeUrl + "/subscription/soapservice/" + assetId, MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_JSON, dataObject.toString(), queryParamMap, headerMap, cookieHeaderStore);
+
+        String payLoad = response.getEntity(String.class);
+        payLoad = payLoad.substring(payLoad.indexOf('{'));
+        JSONObject obj = new JSONObject(payLoad);
+        assertNotNull(obj.get("id").toString(),
+                "Response payload is not the in the correct format" + response.getEntity(String.class));
+
+        genericRestClient.geneticRestRequestPost(publisherUrl + "/assets/" + assetId + "/state",
+                MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON,
+                "nextState=Testing&comment=Completed", queryParamMap, headerMap, cookieHeaderPublisher);
+        // TODO - Since notification not appearing in the store
+
+    }
+
+    @Test(groups = { "wso2.greg",
+            "wso2.greg.es" }, description = "Adding subscription to soap service on resource update")
+    public void addSubscriptionToResourceUpdate() throws JSONException, IOException {
+
+        JSONObject dataObject = new JSONObject();
+        dataObject.put("notificationType", "StoreResourceUpdated");
+        dataObject.put("notificationMethod", "work");
+
+        ClientResponse response = genericRestClient
+                .geneticRestRequestPost(storeUrl + "/subscription/soapservice/" + assetId, MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_JSON, dataObject.toString(), queryParamMap, headerMap, cookieHeaderStore);
+
+        String payLoad = response.getEntity(String.class);
+        payLoad = payLoad.substring(payLoad.indexOf('{'));
+        JSONObject obj = new JSONObject(payLoad);
+        assertNotNull(obj.get("id").toString(),
+                "Response payload is not the in the correct format" + response.getEntity(String.class));
+
+        String dataBody = readFile(resourcePath + "json" + File.separator + "PublisherSoapResourceUpdateFile.json");
+        genericRestClient.geneticRestRequestPost(publisherUrl + "/assets/" + assetId, MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_JSON, dataBody, queryParamMap, headerMap, cookieHeaderPublisher);
+        // TODO - Since notification not appearing in the store
+
+    }
+
+
+    @Test(groups = { "wso2.greg",
+            "wso2.greg.es" }, description = "Adding wrong subscription method to check the error message")
+    public void addWrongSubscriptionMethod() throws JSONException, IOException {
+
+        JSONObject dataObject = new JSONObject();
+        dataObject.put("notificationType", "StoreResourceUpdated");
+        dataObject.put("notificationMethod", "test");
+
+        ClientResponse response = genericRestClient
+                .geneticRestRequestPost(publisherUrl + "/subscriptions/soapservice/" + assetId,
+                        MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, dataObject.toString(), queryParamMap,
+                        headerMap, cookieHeaderPublisher);
+
+        String payLoad = response.getEntity(String.class);
+        payLoad = payLoad.substring(payLoad.indexOf('{'));
+        JSONObject obj = new JSONObject(payLoad);
+        assertNotNull(obj.get("error").toString(),
+                "Error message is not contained in the response for wrong notification method \"test\"" + response
+                        .getEntity(String.class));
+    }
+
+    private void deleteSoapServiceAsset() throws JSONException {
+        genericRestClient.geneticRestRequestDelete(publisherUrl + "/assets/" + assetId, MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_JSON, queryParamMap, headerMap, cookieHeaderPublisher);
+    }
+
+    private void setTestEnvironment() throws JSONException, IOException {
+        // Authenticate Publisher
+        ClientResponse response = genericRestClient
+                .geneticRestRequestPost(publisherUrl + "/authenticate/", MediaType.APPLICATION_FORM_URLENCODED,
+                        MediaType.APPLICATION_JSON, "username=admin&password=admin", queryParamMap, headerMap, null);
+        JSONObject obj = new JSONObject(response.getEntity(String.class));
+        jSessionIdPublisher = obj.getJSONObject("data").getString("sessionId");
+        cookieHeaderPublisher = "JSESSIONID=" + jSessionIdPublisher;
+
+        // Authenticate Store
+        ClientResponse responseStore = genericRestClient
+                .geneticRestRequestPost(storeUrl + "/authenticate/", MediaType.APPLICATION_FORM_URLENCODED,
+                        MediaType.APPLICATION_JSON, "username=admin&password=admin", queryParamMap, headerMap, null);
+        obj = new JSONObject(responseStore.getEntity(String.class));
+        jSessionIdStore = obj.getJSONObject("data").getString("sessionId");
+        cookieHeaderStore = "JSESSIONID=" + jSessionIdStore;
+
+        //Create soap service
+        queryParamMap.put("type", "soapservice");
+        String dataBody = readFile(resourcePath + "json" + File.separator + "publisherPublishSoapResource.json");
+        ClientResponse createResponse = genericRestClient
+                .geneticRestRequestPost(publisherUrl + "/assets", MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_JSON, dataBody, queryParamMap, headerMap, cookieHeaderPublisher);
+        JSONObject createObj = new JSONObject(createResponse.getEntity(String.class));
+        assetId = createObj.get("id").toString();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void clean() throws Exception {
+        deleteSoapServiceAsset();
+    }
+
+    @DataProvider
+    private static TestUserMode[][] userModeProvider() {
+        return new TestUserMode[][]{
+                new TestUserMode[]{TestUserMode.SUPER_TENANT_ADMIN}
+                //                new TestUserMode[]{TestUserMode.TENANT_USER},
+        };
+    }
+}
