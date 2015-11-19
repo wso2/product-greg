a500e21861fa119452a8f33820dcf15f0037fc58
denuwanthi <denuwanthi.hasanthika@gmail.com>
2015-11-19 17:40:55 +0530

    Access methods of EmailUtil class statically

--- modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/notifications/PolicyEmailNotificationTestCase.java
+++ modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/notifications/PolicyEmailNotificationTestCase.java
@@ -65,7 +65,6 @@ import static org.testng.Assert.assertTrue;
 public class PolicyEmailNotificationTestCase extends GregESTestBaseTest {
 
     private TestUserMode userMode;
-    private EmailUtil emailUtil;
     private UserProfileMgtServiceClient userProfileMgtClient;
     private File axis2File;
     private String publisherUrl;
@@ -217,7 +216,7 @@ public class PolicyEmailNotificationTestCase extends GregESTestBaseTest {
         ClientResponse responseCheck0 =
                 checkLifeCycleCheckItem(cookieHeader, 0);
         Assert.assertTrue(responseCheck0.getStatusCode() == 200);
-        isNotificationMailAvailable = emailUtil.readGmailInboxForNotification("PublisherCheckListItemChecked");
+        isNotificationMailAvailable = EmailUtil.readGmailInboxForNotification("PublisherCheckListItemChecked");
         assertTrue(isNotificationMailAvailable,
                    "Publisher check list item ticked on life cycle, notification mail has failed to reach Gmail inbox");
         isNotificationMailAvailable = false;
@@ -256,7 +255,7 @@ public class PolicyEmailNotificationTestCase extends GregESTestBaseTest {
         ClientResponse responseUncheck0 =
                 uncheckLifeCycleCheckItem(cookieHeader, 0);
         Assert.assertTrue(responseUncheck0.getStatusCode() == 200);
-        isNotificationMailAvailable = emailUtil.readGmailInboxForNotification("PublisherCheckListItemUnchecked");
+        isNotificationMailAvailable = EmailUtil.readGmailInboxForNotification("PublisherCheckListItemUnchecked");
         assertTrue(isNotificationMailAvailable,
                    "Publisher uncheck list item ticked on life cycle, notification mail has failed to reach Gmail inbox");
         isNotificationMailAvailable = false;
@@ -295,7 +294,7 @@ public class PolicyEmailNotificationTestCase extends GregESTestBaseTest {
         JSONObject obj = new JSONObject(response.getEntity(String.class));
         String status = obj.get("status").toString();
         Assert.assertEquals(status, STATE_CHANGE_MESSAGE);
-        isNotificationMailAvailable = emailUtil.readGmailInboxForNotification("PublisherLifeCycleStateChanged");
+        isNotificationMailAvailable = EmailUtil.readGmailInboxForNotification("PublisherLifeCycleStateChanged");
         assertTrue(isNotificationMailAvailable,
                    "Publisher lifecycle state changed notification mail has failed to reach Gmail inbox");
         isNotificationMailAvailable = false;
