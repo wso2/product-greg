1d3cac31608fa3b97354c21a3dd4ec9d8ec9dfe1
denuwanthi <denuwanthi.hasanthika@gmail.com>
2015-12-23 17:04:48 +0530

    Add test case for get asset by name

--- modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/governancresteapi/GovernanceRestAPITestCase.java
+++ modules/integration/tests-integration/tests-es/src/test/java/org/wso2/carbon/registry/es/governancresteapi/GovernanceRestAPITestCase.java
@@ -198,8 +198,24 @@ public class GovernanceRestAPITestCase extends GregESTestBaseTest {
         }
     }
 
-    @Test(groups = {"wso2.greg", "wso2.greg.governance.rest.api"}, description = "Update the context of a REST service",
+    @Test(groups = {"wso2.greg", "wso2.greg.governance.rest.api"}, description = "Looks for artifacts with given name",
           dependsOnMethods = {"getListOfAssets"})
+    public void getAssetWithName() throws JSONException {
+
+        String governanceRestApiUrl = governaceAPIUrl + "/restservices";
+        queryParamMap.put("name",restService1Name);
+        ClientResponse clientResponse = genericRestClient.geneticRestRequestGet(governanceRestApiUrl,queryParamMap,
+                                                                                headerMap,null);
+        JSONObject jsonObject = new JSONObject(clientResponse.getEntity(String.class));
+        JSONArray jsonArray = jsonObject.getJSONArray("assets");
+        Assert.assertEquals(jsonArray.getJSONObject(ASSET_ID_ONE_INDEX).get("name"),restService1Name);
+        Assert.assertEquals(jsonArray.getJSONObject(ASSET_ID_ONE_INDEX).get("context"),context1);
+        Assert.assertEquals(jsonArray.getJSONObject(ASSET_ID_ONE_INDEX).get("id"),assetId1);
+        queryParamMap.clear();
+    }
+
+    @Test(groups = {"wso2.greg", "wso2.greg.governance.rest.api"}, description = "Update the context of a REST service",
+          dependsOnMethods = {"getAssetWithName"})
     public void updateAnAsset() throws IOException, JSONException {
 
         String updatedContext = "/rest1-new";
