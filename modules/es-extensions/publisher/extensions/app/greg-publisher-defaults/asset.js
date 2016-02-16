b6c9f8265e520bb62e109030ebe699b30a671cba
Kasun Thennakoon <tmkasun@gmail.com>
2016-02-16 22:09:18 +0530

    show dropdown only if there are more than one different versions

--- modules/es-extensions/publisher/extensions/app/greg-publisher-defaults/asset.js
+++ modules/es-extensions/publisher/extensions/app/greg-publisher-defaults/asset.js
@@ -106,6 +106,11 @@ asset.renderer = function(ctx) {
 
                 var type = page.assets.type;
                 page.assetVersions = gregAPI.getAssetVersions(ctx.session, ctx.assetType, page.assets.path, page.assets.name);
+                var single_version = false;
+                if(page.assetVersions.length === 1){
+                    single_version = true;
+                }
+                page.single_version = single_version;
             },
             decoratingAssetListing: function(page) {
                 // Following is to remove the statistics button in the list page
