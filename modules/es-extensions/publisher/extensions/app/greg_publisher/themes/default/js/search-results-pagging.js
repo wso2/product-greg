d6493376d379cfad5704655371564e2ed8363d07
Thushara Kasun Ranawaka <thushara35@gmail.com>
2016-08-11 16:57:23 +0530

    Fix for REGISTRY-3868

--- modules/es-extensions/publisher/extensions/app/greg_publisher/themes/default/js/search-results-pagging.js
+++ modules/es-extensions/publisher/extensions/app/greg_publisher/themes/default/js/search-results-pagging.js
@@ -193,6 +193,10 @@ $(function () {
                 }
 
             }
+        } else if (q.indexOf(' AND ') > -1 || q.indexOf(' OR ') > -1) {
+            // Note: if user want to do a boolean search within multiple attributes this is the place he has to change.
+            comps = (q + ',').split(','); // Since JSON.parse("[" + q + "]") is not working
+            comps.splice(comps.length - 1, 1); // Removing empty array element
         } else {
             comps = q.split(' ');
         }
